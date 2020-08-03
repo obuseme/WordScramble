20c4e004c9209b963fd183a1db4076af539c92d6
obuseme <obusek@gmail.com>
2020-08-03 19:31:41 -0400

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Andrew Obusek on 8/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
